b679129e9b4fe0345c0ff6b2d34a9418a8f82a1a
Tim Brown <tim@redpill.me.uk>
2017-12-06 12:40:58 +0000

    add scroll-phat-hd/scroll-text.py

---
+++ scroll-phat-hd/scroll-text.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import time
+import signal
+import sys
+import scrollphathd
+from scrollphathd.fonts import font5x7
+
+for line in sys.stdin:
+    scrollphathd.write_string(line.rstrip(), x=17, y=0, font=font5x7, brightness=0.3)
+
+    buffer_length = scrollphathd.get_buffer_shape()[0]
+
+    for i in range(buffer_length+1):
+        scrollphathd.show()
+        scrollphathd.scroll()
+        time.sleep(0.05)
+
+  
+    time.sleep(0.5)
+
