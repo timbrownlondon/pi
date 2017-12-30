a2771f71fb447644e1488c9396dcbad541c95dfc
Tim Brown <tim@redpill.me.uk>
2017-12-30 11:07:49 +0000

    video.py - add separate function for managing each record session - call MP4Box to convert video after capture

--- box-brownie-camera/bin/video.py
+++ box-brownie-camera/bin/video.py
@@ -3,6 +3,7 @@
 import time
 import picamera
 import RPi.GPIO as GPIO
+import subprocess
 import os
 
 # set up button
@@ -10,34 +11,52 @@ button = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# pass this in so can use absolute paths and invoke by /etc/rc.local
+base_dir = os.environ['base_dir']
+
+# output is placed in content/<dir>/<index>.<ext>
+filename = '{}/content/{}/{:04d}.{}'
+
+def shoot_video_for_index(i):
+    camera = picamera.PiCamera(resolution = (640, 640), framerate = 20)
+    camera.start_recording(filename.format(base_dir, 'h264', i, 'h264'))
+    print(filename.format(base_dir, 'h2d4', i, 'h264'))
+
+    # continue recording until we have detected button off
+    # several times (loop for switch bounce protection)
+    button_off_count = 0
+    while button_off_count < 3:
+        camera.wait_recording(0.2)
+        if GPIO.input(button):
+            button_off_count += 1
+
+    camera.stop_recording()
+    camera.close()
+
+    # save an mp4 wrapped version of shot video
+    subprocess.call(['MP4Box',
+                     '-add', 
+                     filename.format(base_dir, 'h264', i, 'h264'),
+                     filename.format(base_dir, 'mp4', i, 'mp4')
+                   ])
+
 try:
     while True:
         # button level is inverted (false means pressed)
         while GPIO.input(button):
             time.sleep(0.1)
 
-        # choose filename to save video to
+        # choose next free filename to save video to
         i = 0
-        while os.path.exists("video%03d.h264" % i):
+        while os.path.exists(filename.format(base_dir, 'h264', i, 'h264')):
             i += 1
 
-        camera = picamera.PiCamera()
-        camera.resolution = (512, 512)
-        camera.start_recording('video%03d.h264' % i)
-        button_off_count = 0
-
-        # record number of times have detected button off as a de-bounce protection
-        while button_off_count < 3:
-            camera.wait_recording(0.2)
-            if GPIO.input(button):
-                button_off_count += 1
-
-        camera.stop_recording()
-
+        shoot_video_for_index(i)
 
 
 
 # hit Ctrl + C to stop program
 except KeyboardInterrupt:
-    GPIO.cleanup() #cleanup GPIO channels
-    print ('program stopped')
+    GPIO.cleanup()
+
+
